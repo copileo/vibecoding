43232574cdd1c63f2d6b7588f97cbbea1751c799
copileo <57446555+copileo@users.noreply.github.com>
2026-07-13 12:15:26 +0100

    Atualiza cache do app de despesas

--- apps/despesas-viagem/sw.js
+++ apps/despesas-viagem/sw.js
@@ -1,5 +1,5 @@
-const CACHE='vibecode-despesas-viagem-v1';
+const CACHE='vibecode-despesas-viagem-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('vibecode-despesas-viagem')).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)))})
+self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)))})
