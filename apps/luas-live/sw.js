f9b6fc8bec9e1d91ec98d4e238a93a765d264041
copileo <57446555+copileo@users.noreply.github.com>
2026-07-28 23:15:36 +0100

    Refresh Luas Live cache for reminders

--- apps/luas-live/sw.js
+++ apps/luas-live/sw.js
@@ -1,4 +1,4 @@
-const CACHE='luas-live-v10';
+const CACHE='luas-live-v11';
 const SHELL=['./','./index.html','./styles.css','./config.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
